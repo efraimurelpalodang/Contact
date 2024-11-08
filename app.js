0647c47e5b2cbd0e2a337c4d48f156c2565a1700
efraim urel <efraimurelpalodang@gmail.com>
2024-11-09 06:56:45 +0800

    menginstall module npm dan (npm init/ menginitstalisazi projek kita menjadi projek npm dulu)

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const contacts = require('./contacts');
+//! mengambil argument dari command line
+const command = process.argv[2];
+// if(command === 'add') {
 
-async function main() {
-    const nama = await contacts.askQuestion('Masukkan Nama anda');
-    const email = await contacts.askQuestion('Masukkan Email anda');
-    const noHp = await contacts.askQuestion('Masukkan Nomor anda');
+// } else if (command === 'remove') {
 
-    contacts.simpanKontak(nama, email, noHp);
-}
+// } else if (command === 'list') {
 
-main();
+// }
 
 
 
@@ -22,18 +20,28 @@ main();
 
 
 
-// rl.question('Masukkan nama anda :  ', nama => {
-//     rl.question('Masukkan nomor hp : ', noHp => {
-//         const contact = {nama, noHp};
-//         const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-//         const contacts = JSON.parse(fileBuffer);
 
-//         contacts.push(contact);
 
-//         fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-        
-//         console.log('Terima kasih sudah memasukka data dan nomor HP !!');
-        
-//         rl.close();
-//     })
-//   });
+
+
+
+
+
+
+
+
+
+
+
+
+// const contacts = require('./contacts');
+
+// async function main() {
+//     const nama = await contacts.askQuestion('Masukkan Nama anda');
+//     const email = await contacts.askQuestion('Masukkan Email anda');
+//     const noHp = await contacts.askQuestion('Masukkan Nomor anda');
+
+//     contacts.simpanKontak(nama, email, noHp);
+// }
+
+// main();
